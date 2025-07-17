660c01f320b532650cbfa8dc47315a770ba3b8ac
YusufRick <c3022740@hallam.shu.ac.uk>
2025-07-17 22:24:49 +0800

    servvices

--- mediliance--website/src/components/NavBar.jsx
+++ mediliance--website/src/components/NavBar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => (
       <div className="logo">Mediliance</div>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/shareholders">Shareholders</Link></li>
-        <li><Link to="/client">Client</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/client">Clients</Link></li>
+        <li><Link to="/contact">Email</Link></li>
       </ul>
     </div>
   </nav>
