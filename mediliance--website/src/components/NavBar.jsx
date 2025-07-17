99b48767d3b70ad0a6a6c2ee20b30bd60363e17d
YusufRick <c3022740@hallam.shu.ac.uk>
2025-07-17 21:45:19 +0800

    improvement

--- mediliance--website/src/components/NavBar.jsx
+++ mediliance--website/src/components/NavBar.jsx
@@ -1,15 +1,16 @@
 // src/components/Navbar.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav>
     <div className="nav-container">
       <div className="logo">Mediliance</div>
       <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#shareholders">Shareholders</a></li>
-        <li><a href="#client">Client</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/shareholders">Shareholders</Link></li>
+        <li><Link to="/client">Client</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </div>
   </nav>
